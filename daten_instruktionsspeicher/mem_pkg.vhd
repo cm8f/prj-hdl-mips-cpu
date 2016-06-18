cb69b5aa5c0721e3e14f4a350cf86fe828f0f65c
Philipp Hecht <philipp.hecht@fau.de>
2016-06-18 16:56:10 +0200

    add first assembler program in instruction memory

--- daten_instruktionsspeicher/mem_pkg.vhd
+++ daten_instruktionsspeicher/mem_pkg.vhd
@@ -7,5 +7,18 @@ package mem_pkg is
 	constant addrw  : integer := 5;
 	
 	type rom_t is array(0 to 2**addrw-1) of std_logic_vector(dwidth-1 downto 0);
-	constant c_instr : rom_t := (others => (others => '0'));
+	--constant c_instr : rom_t := (others => (others => '0'));
+
+    constant reg_incr : rom_t := ( 
+        x"20080024",
+        x"200903e8",
+        x"20190001",
+        x"08100005",
+        x"21080001",
+        x"0109502a",
+        x"1159fffd",
+        others => (others => '0')
+    );
+
+    constant c_instr : rom_t := reg_incr;
 end package;
